63f5b0ef21e48f15978b8a1d4b067248cc458bb6
rizalariadi <hwr.iza@gmail.com>
2025-11-29 11:25:38 +0800

    Session: set expire_on_close=false, adjust idle logout to session lifetime

--- config/session.php
+++ config/session.php
@@ -29,9 +29,9 @@ return [
     |
     */
 
-    'lifetime' => 1,
+    'lifetime' => 30,
 
-    'expire_on_close' => true,
+    'expire_on_close' => false,
 
     /*
     |--------------------------------------------------------------------------
